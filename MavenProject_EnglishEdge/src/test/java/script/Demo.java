b8cc8b1d8912797cbf009dcefcf536551a628415
TYSS <TYSS@TYSS-4F-070>
2018-04-27 13:40:10 +0530

    1st testscript commit

---
+++ MavenProject_EnglishEdge/src/test/java/script/Demo.java
@@ -0,0 +1,14 @@
+package script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+	@Test
+	public void demoTest()
+	{
+		Reporter.log("Demo test",true);
+	}
+
+}
